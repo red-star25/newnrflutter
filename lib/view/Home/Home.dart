0a262f71607f0ecf25d1370bece0e22569b810f1
red-star25 <nakumdhruv123@gmail.com>
2021-03-04 11:36:45 +0530

    Added proguard rules

--- lib/view/Home/Home.dart
+++ lib/view/Home/Home.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:nrlifecare/controller/HomeController/homeController.dart';
@@ -9,48 +11,56 @@ import 'package:nrlifecare/wigdets/HomeWidgets/FloatingButton.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class Home extends StatelessWidget {
+  Future<bool> exitApp() {
+    exit(0);
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        floatingActionButton: FloatingButton(),
-        body: NestedScrollView(
-          controller: Get.find<HomeController>().scrollController,
-          headerSliverBuilder: (context, innerBoxIsScroller) => [
-            SliverOverlapAbsorber(
-              handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context),
-              sliver: SliverSafeArea(
-                top: false,
-                sliver: SliverAppBar(
-                  leading: Container(),
-                  floating: true,
-                  backgroundColor: Colors.white,
-                  expandedHeight: 0.82.sh,
-                  titleSpacing: 0,
-                  flexibleSpace: FlexibleSpaceBar(
-                    background: AnimatedContainer(
-                      // color: Colors.lightBlue,
-                      duration: const Duration(milliseconds: 200),
-                      child: Column(
-                        children: [
-                          CustomAppBar(),
-                          SizedBox(
-                            height: 25.h,
-                          ),
-                          TopProducts(),
-                          SizedBox(
-                            height: 25.h,
-                          ),
-                          Categories(),
-                        ],
+      child: WillPopScope(
+        onWillPop: exitApp,
+        child: Scaffold(
+          floatingActionButton: FloatingButton(),
+          body: NestedScrollView(
+            controller: Get.find<HomeController>().scrollController,
+            headerSliverBuilder: (context, innerBoxIsScroller) => [
+              SliverOverlapAbsorber(
+                handle:
+                    NestedScrollView.sliverOverlapAbsorberHandleFor(context),
+                sliver: SliverSafeArea(
+                  top: false,
+                  sliver: SliverAppBar(
+                    leading: Container(),
+                    floating: true,
+                    backgroundColor: Colors.white,
+                    expandedHeight: 0.82.sh,
+                    titleSpacing: 0,
+                    flexibleSpace: FlexibleSpaceBar(
+                      background: AnimatedContainer(
+                        // color: Colors.lightBlue,
+                        duration: const Duration(milliseconds: 200),
+                        child: Column(
+                          children: [
+                            CustomAppBar(),
+                            SizedBox(
+                              height: 25.h,
+                            ),
+                            TopProducts(),
+                            SizedBox(
+                              height: 25.h,
+                            ),
+                            Categories(),
+                          ],
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-            )
-          ],
-          body: NewIn(),
+              )
+            ],
+            body: NewIn(),
+          ),
         ),
       ),
     );
